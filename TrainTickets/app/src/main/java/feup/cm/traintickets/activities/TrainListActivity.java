8167d17a894028b66a597d56157607b3af8d2293
rolando <rolando.a@sapo.pt>
2017-04-06 16:20:03 +0100

    Train_list_activity

---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/activities/TrainListActivity.java
@@ -0,0 +1,20 @@
+package feup.cm.traintickets.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import feup.cm.traintickets.R;
+
+/**
+ * Created by HP on 06/04/2017.
+ */
+
+public class TrainListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_train_list);
+    }
+}
