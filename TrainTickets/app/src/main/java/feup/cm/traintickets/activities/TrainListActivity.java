bdd67e7e0f5b19df6ff428321480d7c22472e4e4
rolando <rolando.a@sapo.pt>
2017-04-06 19:07:32 +0100

    TrainTrip Model to populate listView of trains

--- TrainTickets/app/src/main/java/feup/cm/traintickets/activities/TrainListActivity.java
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/activities/TrainListActivity.java
@@ -3,18 +3,30 @@ package feup.cm.traintickets.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import feup.cm.traintickets.R;
-
-/**
- * Created by HP on 06/04/2017.
- */
+import feup.cm.traintickets.models.TrainTripModel;
 
 public class TrainListActivity extends AppCompatActivity {
+
+    public ListView listaComboios;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_list);
+
+        ListView listaComboios = (ListView) findViewById(R.id.listViewTrainList);
+
+        List<TrainTripModel> listTrain = new ArrayList<TrainTripModel>();
+
+
     }
+
+
+
 }
---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/models/TrainTripModel.java
@@ -0,0 +1,60 @@
+package feup.cm.traintickets.models;
+
+import java.sql.Time;
+
+public class TrainTripModel {
+
+    private int id;
+    private String description;
+    private Time departureTime;
+    private Time arrivalTime;
+    private int duration;
+
+    public TrainTripModel(int id,String description,Time departureTime,Time arrivalTime,int duration){
+        this.id=id;
+        this.description=description;
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.duration=duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Time getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Time departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Time getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Time arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
