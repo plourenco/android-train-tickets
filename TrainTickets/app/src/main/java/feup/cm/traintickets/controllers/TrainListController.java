999be8f0d399ce487f021a7ae0e988b7df9d7858
rolando <rolando.a@sapo.pt>
2017-04-11 19:40:10 +0100

    trainList Controller, datamanager, e asysnk task

---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/controllers/TrainListController.java
@@ -0,0 +1,11 @@
+package feup.cm.traintickets.controllers;
+
+/**
+ * Created by HP on 11/04/2017.
+ */
+
+public class TrainListController {
+    public String getTripList(String token) {
+        return ServiceHandler.makeGet("timetable/timetables", token);
+    }
+}
---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/runnables/TrainTripGetTask.java
@@ -0,0 +1,7 @@
+package feup.cm.traintickets.runnables;
+
+import android.os.AsyncTask;
+
+public abstract class TrainTripGetTask extends AsyncTask<Void, Void, Boolean> {
+
+}
