04af8194e398b0e25d751cb501511ffe3ec667d9
rolando <rolando.a@sapo.pt>
2017-04-12 21:14:06 +0100

    Adapter, Activity, model for TrainTrip list

--- TrainTickets/app/src/main/java/feup/cm/traintickets/activities/TrainListActivity.java
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/activities/TrainListActivity.java
@@ -3,15 +3,23 @@ package feup.cm.traintickets.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
 import feup.cm.traintickets.R;
+import feup.cm.traintickets.adapters.TrainListAdapter;
 import feup.cm.traintickets.models.TrainTripModel;
 
 public class TrainListActivity extends AppCompatActivity {
+
 
     public ListView listaComboios;
 
@@ -22,11 +30,61 @@ public class TrainListActivity extends AppCompatActivity {
 
         ListView listaComboios = (ListView) findViewById(R.id.listViewTrainList);
 
-        List<TrainTripModel> listTrain = new ArrayList<TrainTripModel>();
+        //List<TrainTripModel> listTrain = new ArrayList<TrainTripModel>();
+
+       TrainTripModel data[]= new TrainTripModel[]
+               {
+                       new TrainTripModel(1,"Morning Train",Time.valueOf("8:00:00"),Time.valueOf("10:00:00"),120),
+                       new TrainTripModel(2,"Noon Train",Time.valueOf("12:00:00"),Time.valueOf("14:00:00"),120),
+                       new TrainTripModel(3,"After Noon Train",Time.valueOf("16:00:00"),Time.valueOf("18:00:00"),120),
+                       new TrainTripModel(4,"Night Train",Time.valueOf("20:00:00"),Time.valueOf("22:00:00"),120)
+               };
+
+        TrainListAdapter adp= new TrainListAdapter(this,R.layout.content_train_list,data);
+        listaComboios.setAdapter(adp);
 
 
     }
 
+    /*
 
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return id.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View listaComboios, ViewGroup parent) {
+            listaComboios =getLayoutInflater().inflate(R.layout.content_train_list,null);
+
+            TextView txtDescription=(TextView)findViewById(R.id.train_ticket_title_txt);
+            TextView txtId=(TextView)findViewById(R.id.train_ticket_id_txt);
+            TextView txtDepartureTime=(TextView)findViewById(R.id.train_list_departure_txt);
+            TextView txtArrivalTime=(TextView)findViewById(R.id.train_list_arrival_txt);
+            TextView txtDuration=(TextView)findViewById(R.id.train_list_duration_txt);
+
+            txtDescription.setText(description[position]);
+            txtId.setText(id[position]);
+            txtDepartureTime.setText(departureTime[position]);
+            txtArrivalTime.setText(arrivalTime[position]);
+            txtDuration.setText(duration[position]);
+
+            return null;
+        }
+
+    }
+    */
 
 }
---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/adapters/TrainListAdapter.java
@@ -0,0 +1,76 @@
+package feup.cm.traintickets.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import feup.cm.traintickets.R;
+import feup.cm.traintickets.models.TicketModel;
+import feup.cm.traintickets.models.TrainTripModel;
+
+public class TrainListAdapter extends ArrayAdapter<TrainTripModel>  {
+
+    private ArrayList<TrainTripModel> dataSet;
+    Context mContext;
+
+    // Layout view lookup
+    public static class ViewHolder{
+        TextView txtTitle;
+        TextView txtId;
+        TextView txtDeparture;
+        TextView txtArrival;
+        TextView txtDuration;
+    }
+
+    public TrainListAdapter(ArrayList<TrainTripModel> data,Context context){
+        super(context,R.layout.row_item,data);
+        this.dataSet=data;
+        this.mContext=context;
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public View getView(int position,View convertView,ViewGroup parent) {
+
+        TrainTripModel dataModel = getItem(position);
+        ViewHolder viewHolder;
+
+        final View result;
+
+        if(convertView==null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.row_item,parent,false);
+            viewHolder.txtTitle=(TextView)convertView.findViewById(R.id.train_ticket_title_txt);
+            viewHolder.txtId=(TextView)convertView.findViewById(R.id.train_ticket_id_txt);
+            viewHolder.txtDeparture=(TextView)convertView.findViewById(R.id.train_list_departure_txt);
+            viewHolder.txtArrival=(TextView)convertView.findViewById(R.id.train_list_arrival_txt);
+            viewHolder.txtDuration=(TextView)convertView.findViewById(R.id.train_list_duration_txt);
+
+            result=convertView;
+            convertView.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder=(ViewHolder)convertView.getTag();
+            result=convertView;
+        }
+
+
+        return convertView;
+    }
+
+
+}
