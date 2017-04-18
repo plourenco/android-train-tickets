15f3a8dad9adefb6ec916923e532b9362bc59886
rolando <rolando.a@sapo.pt>
2017-04-18 01:05:46 +0100

    Layout TimeTable Timetable activity Time table ListView Adapater

---
+++ TrainTickets/app/src/main/java/feup/cm/traintickets/activities/Timetable.java
@@ -0,0 +1,15 @@
+package feup.cm.traintickets.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import feup.cm.traintickets.R;
+
+public class Timetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable);
+    }
+}
